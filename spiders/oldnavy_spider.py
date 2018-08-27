2d977c79cb161afa5251b4b785d63c097b4e6291
samarqusai <samar.kousay@hotmail.com>
2018-08-27 23:53:24 +0200

    Old navy new arrivals section

---
+++ spiders/oldnavy_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class OldnavyScraping(scrapy.Spider):
+    name    = "oldnavy"
+
+    start_urls  = ["https://oldnavy.gap.com/"];
+
+    # New Arrivals section
+    def parse(self, response):
+        # print(response.css('div.toc'));
+        header      =  response.css('div.new-arrivals-container div.new-arrivals-ctas h3 a::text').extract();
+        menuList    = [];
+        for quote in response.css('div.new-arrivals-container div.new-arrivals-ctas ul.horiz_list li'):
+            menuList.append({'menu_url': quote.css('a::attr(href)').extract(), 'menu_name': quote.css('a::text').extract()})
+
+        yield {
+            'header': header,
+            'menu'  : menuList
+        }
